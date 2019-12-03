8c6ab69cdf605b539581f6ddd285ccaba3f81a96
Sky Walker <captainsone24@gmail.com>
2019-12-03 13:13:26 +0800

    add read file

--- products.py
+++ products.py
@@ -1,6 +1,24 @@
 #記帳程式專案
 #使用二維清單紀錄每次購買的商品及價格
+
+#讀取檔案
 products = []
+with open('products.csv','r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #skip 到下一圈
+		#strip()將空格跟分行脫掉
+		#split(',')用逗號切割,切割完的結果是清單
+		#s = line.strip().split(',')
+		#name = s[0]
+		#price = s[1]
+		#原始有1個逗號,2個內容物,也可以如下寫法,分別儲存到2個變數中 
+		name, price = line.strip().split(',')
+		products.append([name,price]) 
+
+print(products)
+
+#讓使用者輸入
 while True:
 	name =  input('請輸入商品名稱(離開請輸入q): ')
 	if name == 'q':
@@ -19,9 +37,11 @@ while True:
 	products.append([name,price]) 
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
